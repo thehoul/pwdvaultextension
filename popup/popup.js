e7518a1df02b6d1af162bc040e51db3016d2e4ed
HOT <theo@houle.pro>
2024-06-26 11:14:30 +0200

    add 2fa button

--- popup/popup.js
+++ popup/popup.js
@@ -17,4 +17,9 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('pwdmanage').addEventListener('click', () => {
         // TODO 
     });
+
+    document.getElementById('enable-2fa').addEventListener('click', () => {
+        // TODO request the qrcode (/2faActivate) 
+        // and show the qrcode in a dialog with a input field for the code and submit and return button
+    });
 });
